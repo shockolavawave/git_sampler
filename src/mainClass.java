b4de1768fe0f627d6ec84d0711993e09d9c44f28
shockolavawave <atharv.sambarat@gmail.com>
2022-02-22 11:57:17 +0530

    initial commit

---
+++ src/mainClass.java
@@ -0,0 +1,5 @@
+public class mainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello! This is a sample project for learning git in integration with intellij");
+    }
+}
